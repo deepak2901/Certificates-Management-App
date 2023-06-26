5f7914ee4fb92b1e262daf35853e717f4ffa24cc
Deepak <deepak.kondapalli29@gmail.com>
2023-06-26 11:29:25 +0530

    need to change the access contorls for user & admin

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { signup, signin, forgetPassword } = require("../controllers/auth");
 const { uploadFiles } = require("../controllers/uploadfiles")
+const { fetchFiles } = require("../controllers/fetchFiles");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
@@ -10,5 +11,6 @@ router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/forget-password", forgetPassword);
 router.post("/uploadfiles", upload.single("filename"), uploadFiles);
+router.get("/files", fetchFiles)
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // Replace '*' with your specific origin or origins
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+//   });
 
 // Routes
 app.use("/api", authRoutes);
